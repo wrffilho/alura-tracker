4c6ae39e421ff0abd3e0023291d872a65ed137b1
Wanderlei Rocha Filho <wanderleirochafilho@gmail.com>
2023-04-10 01:05:19 -0400

    first commit

---
+++ src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <main class="columns is-gapless is-multiline">
+    <div class="column is-one-quarter"></div>
+    <div class="column is-three-quarter"></div>
+  </main>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+
+export default defineComponent({
+  name: 'App',
+
+});
+</script>
+
+<style>
+header {
+  padding: 1rem;
+  background: #0d3b66;
+  width: 100%;
+  height: 100vh;
+}
+
+@media only screen and (max-width: 768px) {
+  header {
+    padding: 2.5rem;
+    height: auto;
+  }
+}
+</style>
---
+++ src/components/BarraLateral.vue
@@ -0,0 +1,29 @@
+<template>
+
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+
+export default defineComponent({
+  name: 'App',
+
+});
+</script>
+
+<style>
+header {
+  padding: 1rem;
+  background: #0d3b66;
+  width: 100%;
+  height: 100vh;
+}
+
+@media only screen and (max-width: 768px) {
+  header {
+    padding: 2.5rem;
+    height: auto;
+  }
+}
+</style>
